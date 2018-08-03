64122902a738702e7556d941121010fd41ac2eb3
AlexDuOl <contactogatovagabundo@gmail.com>
2018-08-03 13:20:18 -0500

    Subiendo cambios en css, index, js

--- js/app.js
+++ js/app.js
@@ -11,21 +11,17 @@ $(document).ready(function(){
            console.log(data);
            
            let arrayPokemon = data.pokemon_entries;
-           console.log(arrayPokemon);
-   
-          
+            
                arrayPokemon.forEach(function(element){
                let name = element.pokemon_species.name;
                let number = element.entry_number;
                let photo = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${number}.png`;
                
                $("#response-container").append(template(name, photo));
-            
    
-           })
-   
-                
-       })
+           });
+               
+       });
        const template = function (name, photo) {
            const templatePok = `<tr>
                        <td><img src="${photo}"></td>
@@ -34,6 +30,30 @@ $(document).ready(function(){
            return templatePok; 
               
        }
-})
 
+          
+        $("#searching-Pokemon").click(function() {
+
+            dir = "https://pokeapi.co/api/v2/pokemon/1/"
+   
+       $.ajax({
+           url:dir,
+           onError:function (err) {
+               alert("No se ha podido cargar la informacion");            
+           },
+   
+       }).done(function (data){
+           console.log(data);
+
+          let arrayData = data;
+            
+               arrayData.forEach(function(element){
+               let dataPok = element.data.name;
+
+             $('#poke').html(dataPok);
+        
+      })
+    })
+    })  
+})       
 
